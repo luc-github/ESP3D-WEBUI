8a8b924f639810f163814cf61ffad3c09f6089f5
Luc <luc@tech-hunters.com>
2019-04-19 10:47:16 +0200

    Add browser info in status - used for captive portal

--- www/js/statusdlg.js
+++ www/js/statusdlg.js
@@ -1,11 +1,37 @@
+var statuspage = 0;
+var statuscontent = "";
 //status dialog
 function statusdlg () {
     var modal = setactiveModal('statusdlg.html');
     if ( modal == null) return;
     showModal() ;
     refreshstatus ();
+    update_btn_status(0);
 }
 
+function next_status(){
+    var modal = getactiveModal();
+    var text = modal.element.getElementsByClassName("modal-text")[0];
+    if (statuspage == 0){
+        text.innerHTML= statuscontent;
+    } else {
+        text.innerHTML= "<table><tr><td width='auto' style='vertical-align:top;'><label translate>Browser: </label></td><td>&nbsp;</td><td width='100%'><span class='text-info'><strong>"+navigator.userAgent+"</strong></span></td></tr></table>";
+    }
+    update_btn_status();
+}
+
+function update_btn_status(forcevalue){
+  if(typeof forcevalue !== 'undefined'){
+      statuspage = forcevalue;
+  }
+  if(statuspage == 0){
+      statuspage = 1;
+      document.getElementById('next_status_btn').innerHTML = get_icon_svg("triangle-right","1em" ,"1em")
+  } else {
+      statuspage = 0;
+      document.getElementById('next_status_btn').innerHTML = get_icon_svg("triangle-left","1em","1em")
+  }
+}
 
 function statussuccess(response){
         document.getElementById('refreshstatusbtn').style.display='block';
@@ -14,24 +40,25 @@ function statussuccess(response){
         if ( modal == null) return;
         var text = modal.element.getElementsByClassName("modal-text")[0];
         var tresponse = response.split("\n");
-        var output = "";
+        statuscontent = "";
         for (var i = 0; i < tresponse.length; i++){
             var data = tresponse[i].split(":");
             if (data.length >= 2)  {
-                output+= "<label>" + translate_text_item(data[0]) + ": </label>&nbsp;<span class='text-info'><strong>";
+                statuscontent+= "<label>" + translate_text_item(data[0]) + ": </label>&nbsp;<span class='text-info'><strong>";
                 var data2 = data[1].split(" (")
-                output+= translate_text_item(data2[0].trim());
+                statuscontent+= translate_text_item(data2[0].trim());
                  for (v=1;v<data2.length;v++){
-                    output+=" (" + data2[v];
+                    statuscontent+=" (" + data2[v];
                 }
                 for (v=2;v<data.length;v++){
-                    output+=":" + data[v];
+                    statuscontent+=":" + data[v];
                 }
-                output+="</strong></span><br>";
+                statuscontent+="</strong></span><br>";
             }
-            else output+= tresponse[i]+ "<br>";
+            else statuscontent+= tresponse[i]+ "<br>";
             }
-        text.innerHTML= output;
+        text.innerHTML= statuscontent;
+        update_btn_status(0);
         //console.log(response);
 }
 
