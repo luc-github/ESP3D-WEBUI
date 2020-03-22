26df66ea0ae2c36e8dfe0ad329191c3aeab238db
Luc <8822552+luc-github@users.noreply.github.com>
2020-03-22 15:14:57 +0100

    Use express and express-ws to simulate ESP3D web server and websocket

---
+++ src/server/index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+/*
+ * Web Server for development 
+ * Web Socket server for development
+*/
+
+const WebSocket = require('ws');
+var currentID = 0;
+const app = express();
+let firstconnection = true;
+app.use(express.static('dist'));
+app.get('/command', function (req, res) {
+                var url = req.originalUrl
+                if (url.indexOf("ESP800") != -1) {
+                    res.json({
+                        FWVersion: "3.0.0.a27",
+                        FWTarget: "???",
+                        SDConnection: "None",
+                        Authentication: "Disabled",
+                        WebCommunication: "Synchronous",
+                        WebSocketIP: "localhost",
+                        WebSocketport: "81",
+                        Hostname: "esp3d",
+                        WiFiMode: "STA",
+                        WebUpdate: "Enabled",
+                        Filesystem: "SPIFFS",
+                        Time: "None",
+                    })
+                } else {
+                    res.json({ custom: "unknown query" })
+                }
+                });
+
+app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
+
+var WebSocketServer = require('ws').Server,
+  wss = new WebSocketServer({port: 81})
+
+wss.on('connection', function (ws) {
+  console.log("New connection");
+  ws.send(`currentID:${currentID}`);
+  wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+         client.send(`activeID:${currentID}`);
+      }
+    });
+  currentID++;
+  ws.on('message', function (message) {
+    console.log('received: %s', message)
+  })
+})
--- webpack/development.js
+++ webpack/development.js
@@ -4,39 +4,14 @@ import environment from "./environment"
 module.exports = merge(environment, {
     devtool: "inline-source-map",
     devServer: {
-        before: function(app, server, compiler) {
-            //use this part to simulate http response
-            app.get("/command", function(req, res) {
-                var url = req.originalUrl
-                if (url.indexOf("ESP800") != -1) {
-                    res.json({
-                        FWVersion: "3.0.0.a27",
-                        FWTarget: "???",
-                        SDConnection: "None",
-                        Authentication: "Disabled",
-                        WebCommunication: "Synchronous",
-                        WebSocketIP: "192.168.1.43",
-                        WebSocketport: "81",
-                        Hostname: "esp3d",
-                        WiFiMode: "STA",
-                        WebUpdate: "Enabled",
-                        Filesystem: "SPIFFS",
-                        Time: "None",
-                    })
-                } else {
-                    res.json({ custom: "unknown query" })
-                }
-            })
-            app.get("/camera", function(req, res) {
-                //res.send("You should not be there");
-                res.redirect("/")
-            })
-            app.get("/login", function(req, res) {
-                res.redirect("/")
-            })
-            app.get("/settings", function(req, res) {
-                res.redirect("/")
-            })
-        },
+    port: 3000,
+    open: true,
+    proxy: {
+      '/command': 'http://localhost:8080',
+      '/': {
+       target: 'ws://localhost:81',
+       ws: true // important
     },
+    }
+  },
 })
