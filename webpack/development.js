3f1ccf692417244ac52d4e4f1f2fd3eec339b0f5
Luc <8822552+luc-github@users.noreply.github.com>
2020-03-23 17:48:00 +0100

    Add public directory support for offline files like preferences.json

--- webpack/development.js
+++ webpack/development.js
@@ -1,9 +1,11 @@
 import merge from "webpack-merge"
 import environment from "./environment"
+var path = require('path');
 
 module.exports = merge(environment, {
     devtool: "inline-source-map",
     devServer: {
+        contentBase: path.join(__dirname, '../src/server/public/'),
         port: 3000,
         open: true,
         proxy: {
