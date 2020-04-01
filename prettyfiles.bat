ae5735d604b55116c924dc8836fc674efce88799
Luc <8822552+luc-github@users.noreply.github.com>
2020-04-01 17:53:17 +0200

    add scss in prettier

--- prettyfiles.bat
+++ prettyfiles.bat
@@ -1,4 +1,5 @@
 cd %~dp0
 cmd.exe /c prettier --config .prettierrc --write "{src/**/,webpack/**/}*.js"
+cmd.exe /c prettier --config .prettierrc --write "src/stylesheets/**/*.scss"
 cmd.exe /c prettier --config .prettierrc --write "src/**/*.html"
 pause
