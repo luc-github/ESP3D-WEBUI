c76c9bee31607134829eb1608c45b7faf45e52b7
Luc <8822552+luc-github@users.noreply.github.com>
2020-04-30 14:35:35 +0200

    Add language packs for printer and grbl using specifics translations files

--- src/languages/build.js
+++ src/languages/build.js
@@ -1,6 +1,6 @@
 const fs = require("fs")
 const { Gzipper } = require("gzipper")
-const targetDir = "./dist/languages"
+const targetDir = "./dist/"
 const sourceDir = "./src/languages/"
 
 function deleteFolderRecursive(path) {
@@ -21,21 +21,59 @@ function deleteFolderRecursive(path) {
         fs.rmdirSync(path)
     } else console.log(`No directory "${path}"...`)
 }
-function build() {
-    console.info("Building language packs")
-    deleteFolderRecursive(targetDir)
+function build(directory) {
+    console.info("Building language packs for " + directory)
+    let targetD = targetDir + directory + "/languages/"
+    deleteFolderRecursive(targetD)
     fs.readdir(sourceDir, (err, files) => {
         files.forEach(file => {
             if (file.endsWith(".json") && file != "language-list.json") {
-                let gzipper = new Gzipper(sourceDir + file, targetDir, "")
+                console.log("opening" + sourceDir + file)
+                const data = fs.readFileSync(sourceDir + file, "UTF-8")
+                const lines = data.split(/\r?\n/)
+                if (!fs.existsSync(targetD)) {
+                    fs.mkdirSync(targetD)
+                }
+                var destfile = fs.createWriteStream(targetD + file, {
+                    flags: "w+",
+                })
+                lines.forEach(line => {
+                    if (line.indexOf("}") == -1 && line.length > 0) {
+                        destfile.write(line.trim() + "\r\n")
+                    }
+                })
+                const source2 = "./src/languages/" + directory + "/" + file
+                if (fs.existsSync(source2)) {
+                    const data2 = fs.readFileSync(source2, "UTF-8")
+                    const lines2 = data2.split(/\r?\n/)
+                    let newline = true
+                    lines2.forEach(line2 => {
+                        if (
+                            line2.indexOf("}") == -1 &&
+                            line2.indexOf("{") == -1 &&
+                            line2.length > 0
+                        ) {
+                            if (newline) {
+                                destfile.write(",")
+                                newline = false
+                            }
+                            destfile.write(line2.trim() + "\r\n")
+                        }
+                    })
+                } else {
+                    console.log("No file: " + source2)
+                }
+                destfile.write("}\r\n")
+                destfile.end()
+                let gzipper = new Gzipper(targetD + file, targetD, "")
                 gzipper
                     .compress()
                     .then(files => console.info("Compressed files: ", files))
                     .catch(err => console.error(err))
-                // console.log(file);
             }
         })
     })
 }
 
-build()
+build("printer")
+build("grbl")
