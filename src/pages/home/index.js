7b7b38d687611583de3878a0017e74cb19dba5b3
Alexandre <3979539+alxblog@users.noreply.github.com>
2020-11-09 12:26:57 +0100

    Update Home Comp, for testing context purpose

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,10 +1,19 @@
 import { h } from 'preact';
+import { useContext } from 'preact/hooks'
+import { UiContext } from '../../components/UiContext'
 
-const Home = () => (
-	<div>
-		<h2>Home</h2>
-		<p>This is the Home component.</p>
-	</div>
-)
+const Home = () => {
+	const store = useContext(UiContext)
+	const { addModal } = store.modals
+	const { addToast } = store.toasts
+	return (
+		<div className="container">
+			<h2>Home</h2>
+			<p>This is the Home component.</p>
+			<button onClick={() => { addModal({ size: 'sm', title: 'Title', content: 'Content', footer: <p>Mon composant footer</p> }) }}>Change modal</button>
+			<button onClick={() => { addToast({ content: Date.now(), type: '' }) }}>Open Toast</button>
+		</div>
+	)
+}
 
 export default Home
