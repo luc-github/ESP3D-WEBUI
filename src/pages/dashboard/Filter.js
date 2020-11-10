ce7b164fd8c250190ac159960d795c98c677c364
Alexandre <3979539+alxblog@users.noreply.github.com>
2020-11-10 22:01:25 +0100

    Add filter dropdown + Code rewriting
    Dashboard panels can now be filtered and implicitly reordered.

--- src/pages/dashboard/Filter.js
+++ src/pages/dashboard/Filter.js
@@ -1,29 +1,29 @@
 import { h } from 'preact';
-import { useEffect, useCallback, useRef, useState } from 'preact/hooks';
-
-
-export const Filter = ({ items, children }) => {
-    const dropdownRef = useRef(null);
-    const [isActive, setIsActive] = useState(false);
-    const toggleDropdown = () => setIsActive(!isActive);
-
+import { useRef } from 'preact/hooks'
+export const Filter = ({ items, activePanels, children, action }) => {
+    const dropdownRef = useRef(null)
+    const toggleDropdown = () => dropdownRef.current.focus()
     return (
-        <div className={`dropdown ${isActive ? "active" : ''}`} ref={dropdownRef}>
-            <button className="btn  btn-primary dropdown-toggle" onClick={toggleDropdown}>
-                dropdown menu <i className="icon icon-caret" />
+        <div className="dropdown" >
+            <button className="btn btn-primary dropdown-toggle" onClick={toggleDropdown} ref={dropdownRef}>
+                Panels settings <i className="icon icon-caret" />
             </button>
-            {isActive && (
-                <ul className="menu">
-                    {Object.keys(items).map(item => (
+            <ul className="menu">
+                {Object.keys(items).map((item) => {
+                    return (
                         <li className="menu-item">
                             <label className="form-checkbox">
-                                <input type="checkbox" />
+                                <input type="checkbox"
+                                    value={item}
+                                    checked={activePanels.includes(item)}
+                                    onChange={(e) => { action(e) }} />
                                 <i className="form-icon" /> {item}
                             </label>
-                        </li>))}
-                    {[...children].map(child => (<li className="menu-item">{child}</li>))}
-                </ul>
-            )}
+                        </li>)
+                })}
+                {[...children].map(child => (<li className="menu-item">{child}</li>))}
+            </ul>
+
         </div>
     )
 }
