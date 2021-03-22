4c22b9137b5a1f7151f17c3a3c285fee53fcc350
Luc <luc.@tech-hunters.com>
2021-03-22 16:27:12 +0100

    clean about page a little
    add browser detection in about

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -20,7 +20,7 @@
 */
 import { h } from "preact";
 import { useEffect, useState } from "preact/hooks";
-import { Loading, Panel } from "../../components/Spectre";
+import { Loading } from "../../components/Spectre";
 import { useHttpQueue } from "../../hooks";
 import { espHttpURL } from "../../components/Helpers";
 import { T } from "../../components/Translations";
@@ -52,36 +52,64 @@ const About = () => {
       }
     );
   };
+  //from https://stackoverflow.com/questions/5916900/how-can-you-detect-the-version-of-a-browser
+  function getBrowserInformation() {
+    var ua = navigator.userAgent,
+      tem,
+      M =
+        ua.match(
+          /(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i
+        ) || [];
+    if (/trident/i.test(M[1])) {
+      tem = /\brv[ :]+(\d+)/g.exec(ua) || [];
+      return "IE " + (tem[1] || "");
+    }
+    if (M[1] === "Chrome") {
+      tem = ua.match(/\b(OPR|Edge)\/(\d+)/);
+      if (tem != null) return tem.slice(1).join(" ").replace("OPR", "Opera");
+    }
+    M = M[2] ? [M[1], M[2]] : [navigator.appName, navigator.appVersion, "-?"];
+    if ((tem = ua.match(/version\/(\d+)/i)) != null) M.splice(1, 1, tem[1]);
+    return M.join(" ");
+  }
   useEffect(() => {
     getProps();
   }, []);
   return (
     <div id="about" class="container">
-      <h2>About</h2>
+      <h2>{T("S12")}</h2>
       {isLoading && <Loading />}
-      <Panel>
-        <Panel.Body>
-          {!isLoading && props && (
-            <ul>
-              {props.map(({ id, value }) => (
+
+      {!isLoading && props && (
+        <div>
+          <hr />
+          <center>
+            <div style="display: inline-block;text-align: left;">
+              <ul>
                 <li>
-                  <span class="text-primary">{T(id)} </span> :{" "}
-                  <span class="text-dark">{value}</span>
+                  <span class="text-primary">{T("S18")} </span> :{" "}
+                  <span class="text-dark">{getBrowserInformation()}</span>
                 </li>
-              ))}
-            </ul>
-          )}
-          <button
-            className="btn"
-            onClick={() => {
-              getProps();
-            }}
-          >
-            Refresh
-          </button>
-        </Panel.Body>
-        <Panel.Footer></Panel.Footer>
-      </Panel>
+                {props.map(({ id, value }) => (
+                  <li>
+                    <span class="text-primary">{T(id)} </span> :{" "}
+                    <span class="text-dark">{value}</span>
+                  </li>
+                ))}
+              </ul>{" "}
+            </div>
+            <hr />
+            <button
+              className="btn"
+              onClick={() => {
+                getProps();
+              }}
+            >
+              {T("S50")}
+            </button>
+          </center>
+        </div>
+      )}
     </div>
   );
 };
