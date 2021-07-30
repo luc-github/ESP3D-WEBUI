edc83aa4f9652140415cda39ec9ff4df1a1f44eb
Luc <8822552+luc-github@users.noreply.github.com>
2021-07-30 20:26:33 +0200

    Put extra pages in a drop down menu to avoid overload if many pages

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -19,8 +19,8 @@
  License along with This code; if not, write to the Free Software
  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
-import { h } from "preact";
-import { useState, useEffect } from "preact/hooks";
+import { Fragment, h } from "preact";
+import { useState, useEffect, useRef } from "preact/hooks";
 import { ESP3DLogo, iconsList } from "../Images";
 import { Link } from "../Router";
 import { T } from "../Translations";
@@ -29,21 +29,43 @@ import {
   useUiContext,
   useWsContext,
   useRouterContext,
+  useDatasContext,
 } from "../../contexts";
 import { useHttpQueue } from "../../hooks";
 import { espHttpURL } from "../Helpers";
 import { showConfirmationModal } from "../Modal";
-import { Server, Settings, Activity, LogOut } from "preact-feather";
+import {
+  Server,
+  Settings,
+  Activity,
+  LogOut,
+  Trello,
+  ChevronDown,
+} from "preact-feather";
 
 /*
  * Local const
  *
  */
 const defaultLinks = [
-  { label: <ESP3DLogo bgcolor="#ffffff" />, icon: null, href: "/about" },
+  {
+    label: <ESP3DLogo bgcolor="#ffffff" />,
+    icon: null,
+    href: "/about",
+  },
   { label: "S123", icon: <Activity />, href: "/informations" },
-  { label: "S13", icon: <Server />, href: "/dashboard", id: "dashboardLink" },
-  { label: "S14", icon: <Settings />, href: "/settings", id: "settingsLink" },
+  {
+    label: "S13",
+    icon: <Server />,
+    href: "/dashboard",
+    id: "dashboardLink",
+  },
+  {
+    label: "S14",
+    icon: <Settings />,
+    href: "/settings",
+    id: "settingsLink",
+  },
 ];
 
 const Navbar = () => {
@@ -54,6 +76,16 @@ const Navbar = () => {
   const { createNewRequest } = useHttpQueue();
   const [showConfirmation, setShowConfirmation] = useState(false);
   const { Disconnect } = useWsContext();
+  const { testUI } = useDatasContext();
+  const buttonExtraPage = useRef();
+  const menuExtraPage = useRef();
+  const buttontext = T("S155");
+  const [textbutton, setTextButton] = useState(
+    <Fragment>
+      <Trello />
+      <label class="hide-low">{buttontext}</label>
+    </Fragment>
+  );
 
   function onResize() {
     //if infopage is visible but we are not in mobile view
@@ -87,9 +119,51 @@ const Navbar = () => {
       }
     );
   };
+  const menuLinks = [];
+  if (Object.keys(connectionSettings.current).length > 0) {
+    if (getInterfaceValue("showextrapanels")) {
+      const extraPanels = getInterfaceValue("extrapanels");
+
+      const extraPages = extraPanels.reduce((acc, curr) => {
+        const item = curr.value.reduce((accumulator, current) => {
+          accumulator[current.name] = current.initial;
+          return accumulator;
+        }, {});
+
+        if (item.target == "page") {
+          const pageIcon = iconsList[item.icon] ? iconsList[item.icon] : "";
+          acc.push({
+            label: item.name,
+            icon: pageIcon,
+            href: "/#/extrapage/" + curr.id,
+            id: curr.id,
+          });
+        }
+
+        return acc;
+      }, []);
+      menuLinks.push(...extraPages);
+    }
+  }
   useEffect(() => {
     new ResizeObserver(onResize).observe(document.getElementById("app"));
   }, []);
+
+  useEffect(() => {
+    if (activeRoute.includes("extrapage")) {
+      if (buttonExtraPage.current)
+        buttonExtraPage.current.classList.add("active");
+      menuLinks.map(({ label, icon, href, id }) => {
+        if (href.includes(activeRoute))
+          setTextButton(
+            <Fragment>
+              {icon}
+              <label class="hide-low">{label}</label>
+            </Fragment>
+          );
+      });
+    }
+  }, [activeRoute]);
 
   if (showConfirmation) {
     const title = T("S26");
@@ -112,57 +186,89 @@ const Navbar = () => {
   };
 
   if (Object.keys(connectionSettings.current).length > 0) {
-    const menuLinks = [...defaultLinks];
-    if (getInterfaceValue("showextrapanels")) {
-      const extraPanels = getInterfaceValue("extrapanels");
-
-      const extraPages = extraPanels.reduce((acc, curr) => {
-        const item = curr.value.reduce((accumulator, current) => {
-          accumulator[current.name] = current.initial;
-          return accumulator;
-        }, {});
-
-        if (item.target == "page") {
-          const pageIcon = iconsList[item.icon] ? iconsList[item.icon] : "";
-          acc.push({
-            label: item.name,
-            icon: pageIcon,
-            href: "/extrapage/" + curr.id,
-            id: curr.id,
-          });
-        }
-
-        return acc;
-      }, []);
-      menuLinks.push(...extraPages);
-    }
-
     return (
       <header class="navbar">
         <section class="navbar-section">
-          {menuLinks &&
-            menuLinks.map(({ label, icon, href, id }) => (
-              <Link
-                id={id}
-                className={
-                  href == "/about"
-                    ? "navbar-brand logo no-box "
-                    : connectionSettings.current.FWTarget == 0 &&
-                      href == "/dashboard"
-                    ? "d-none"
-                    : href == "/informations"
-                    ? "btn btn-link no-box feather-icon-container show-low"
-                    : "btn btn-link no-box feather-icon-container"
-                }
-                activeClassName="active"
-                href={href}
+          {defaultLinks &&
+            defaultLinks.map(({ label, icon, href, id }) => {
+              return (
+                <Link
+                  onclick={(e) => {
+                    setTextButton(
+                      <Fragment>
+                        <Trello />
+                        <label class="hide-low">{buttontext}</label>
+                      </Fragment>
+                    );
+                    if (buttonExtraPage.current)
+                      buttonExtraPage.current.classList.remove("active");
+                  }}
+                  id={id}
+                  className={
+                    href == "/about"
+                      ? "navbar-brand logo no-box "
+                      : connectionSettings.current.FWTarget == 0 &&
+                        href == "/dashboard"
+                      ? "d-none"
+                      : href == "/informations"
+                      ? "btn btn-link no-box feather-icon-container show-low"
+                      : "btn btn-link no-box feather-icon-container"
+                  }
+                  activeClassName="active"
+                  href={href}
+                >
+                  {icon}
+                  <label class={href == "/about" ? "" : "hide-low"}>
+                    {T(label)}
+                  </label>
+                </Link>
+              );
+            })}
+          {menuLinks && menuLinks.length > 0 && (
+            <div class="dropdown">
+              <a
+                class="btn btn-link no-box dropdown-toggle feather-icon-container"
+                ref={buttonExtraPage}
+                onclick={(e) => {
+                  if (menuExtraPage.current)
+                    menuExtraPage.current.classList.remove("d-none");
+                }}
+                tabindex="0"
               >
-                {icon}
-                <label class={href == "/about" ? "" : "hide-low"}>
-                  {T(label)}
-                </label>
-              </Link>
-            ))}
+                {textbutton}
+                <ChevronDown />
+              </a>
+              <ul class="menu" ref={menuExtraPage}>
+                {menuLinks &&
+                  menuLinks.map(({ label, icon, href, id }) => {
+                    return (
+                      <li class="menu-item">
+                        <a
+                          id={id}
+                          class="feather-icon-container"
+                          href={href}
+                          onclick={(e) => {
+                            if (menuExtraPage.current)
+                              menuExtraPage.current.classList.add("d-none");
+                            if (buttonExtraPage.current)
+                              buttonExtraPage.current.classList.add("active");
+                            setTextButton(
+                              <Fragment>
+                                {icon}
+                                <label class="hide-low">{label}</label>
+                              </Fragment>
+                            );
+                          }}
+                        >
+                          {icon}
+                          <label>{label}</label>
+                        </a>
+                      </li>
+                    );
+                  })}
+              </ul>
+            </div>
+          )}
         </section>
         <section class="navbar-section">
           <span
