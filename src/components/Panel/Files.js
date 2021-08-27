0007ae699ee9837e3e6f5b9fdd030c830a96568f
Luc <8822552+luc-github@users.noreply.github.com>
2021-08-27 14:21:26 +0200

    Base UI for file panel
    Fix unwished cursor blinking in panels
    Add click effect on panel menu

--- src/components/Panel/Files.js
+++ src/components/Panel/Files.js
@@ -19,7 +19,13 @@ Files.js - ESP3D WebUI component file
 import { h } from "preact";
 import { useEffect } from "preact/hooks";
 import { T } from "../Translations";
-import { MoreVertical, HardDrive } from "preact-feather";
+import {
+  ChevronDown,
+  HardDrive,
+  Upload,
+  RefreshCcw,
+  FolderPlus,
+} from "preact-feather";
 import { useUiContext } from "../../contexts";
 
 /*
@@ -38,9 +44,67 @@ const FilesPanel = () => {
             <HardDrive />
             <strong class="text-ellipsis">{T("S65")}</strong>
           </span>
+
           <span class="navbar-section">
             <span style="height: 100%;">
-              <button
+              <div class="dropdown dropdown-right">
+                <span
+                  class="dropdown-toggle btn btn-xs btn-header m-1"
+                  tabindex="0"
+                >
+                  <ChevronDown size="0.8rem" />
+                </span>
+
+                <ul class="menu">
+                  <li class="menu-item">
+                    <div
+                      class="menu-entry"
+                      onclick={(e) => {
+                        console.log("Create directory");
+                      }}
+                    >
+                      <div class="menu-panel-item">
+                        <span class="text-menu-item">{T("S90")}</span>
+                        <span class="feather-icon-container">
+                          <FolderPlus size="0.8rem" />
+                        </span>
+                      </div>
+                    </div>
+                  </li>
+                  <li class="menu-item">
+                    <div
+                      class="menu-entry"
+                      onclick={(e) => {
+                        console.log("Upload clicked");
+                      }}
+                    >
+                      <div class="menu-panel-item">
+                        <span class="text-menu-item">{T("S89")}</span>
+                        <span class="feather-icon-container">
+                          <Upload size="0.8rem" />
+                        </span>
+                      </div>
+                    </div>
+                  </li>
+                  <li class="divider" />
+                  <li class="menu-item">
+                    <div
+                      class="menu-entry"
+                      onclick={(e) => {
+                        console.log("Refresh clicked");
+                      }}
+                    >
+                      <div class="menu-panel-item">
+                        <span class="text-menu-item">{T("S50")}</span>
+                        <span class="feather-icon-container">
+                          <RefreshCcw size="0.8rem" />
+                        </span>
+                      </div>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+              <span
                 class="btn btn-clear btn-close m-1"
                 aria-label="Close"
                 onclick={(e) => {
@@ -50,7 +114,18 @@ const FilesPanel = () => {
             </span>
           </span>
         </div>
-        <div class="panel-body panel-body-dashboard">Files sytem</div>
+        <div class="input-group m-2">
+          <span>
+            <select class="form-select">
+              <option value="FS">{T("S137")}</option>
+              <option value="SD">{T("S138")}</option>
+            </select>
+          </span>
+        </div>
+        <div class="panel-body panel-body-dashboard files-list m-2">
+          Files sytem
+        </div>
+        <div class="panel-footer files-list-footer">status</div>
       </div>
     </div>
   );
