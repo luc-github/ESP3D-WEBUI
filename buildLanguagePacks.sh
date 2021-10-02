29c1ebb967e63e0c8dc442604246da87d6658ae5
Luc <8822552+luc-github@users.noreply.github.com>
2021-10-02 10:04:04 +0800

    Add shell script for linux env to build  index.html.gz
    Fix typo for grbl
    Bump version

---
+++ buildLanguagePacks.sh
@@ -0,0 +1,49 @@
+cd "$(dirname "$0")"
+npm install
+rm -fr languages
+mkdir -p languages/en
+gulp package --lang en
+cp index.html.gz languages/en
+mkdir -p languages/fr
+gulp package --lang fr
+cp index.html.gz languages/fr
+mkdir -p languages/es
+gulp package --lang es
+cp index.html.gz languages/es
+mkdir -p languages/it
+gulp package --lang it
+cp index.html.gz languages/it
+mkdir -p languages/ja
+gulp package --lang ja
+cp index.html.gz languages/ja
+mkdir -p languages/hu
+gulp package --lang hu
+cp index.html.gz languages/hu
+mkdir -p languages/de
+gulp package --lang de
+cp index.html.gz languages/de
+mkdir -p languages/pl
+gulp package --lang pl
+cp index.html.gz languages/pl
+mkdir -p languages/ptbr
+gulp package --lang ptbr
+cp index.html.gz languages/ptbr
+mkdir -p languages/ru
+gulp package --lang ru
+cp index.html.gz languages/ru
+mkdir -p languages/tr
+gulp package --lang tr
+cp index.html.gz languages/tr
+mkdir -p languages/zh_CN 
+gulp package --lang zh_CN 
+cp index.html.gz languages/zh_CN 
+mkdir -p languages/uk
+gulp package --lang uk
+cp index.html.gz languages/uk
+mkdir -p languages/grbl
+gulp package --lang uk
+cp index.html.gz languages/grbl
+gulp package
+mkdir -p languages/multi
+cp index.html.gz languages/multi
+read -p "Press any key to close ..."
--- www/js/app.js
+++ www/js/app.js
@@ -554,7 +554,7 @@ function initUI() {
     if (typeof document.getElementById('FW_VERSION') != "undefined") document.getElementById('FW_VERSION').innerHTML = fw_version;
     // Get the element with id="defaultOpen" and click on it
     document.getElementById("maintablink").click();
-    if ((target_firmware == "grbl-embedded") || (target_firmware == "grbl-embedded")) {
+    if ((target_firmware == "grbl-embedded") || (target_firmware == "grbl")) {
         if (typeof document.getElementById("grblcontroltablink") !== 'undefined') {
             document.getElementById("grblcontroltablink").click();
         }
