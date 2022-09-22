b77bbf6cabf43ff4e90708ea1c5738a4c489a224
Luc <8822552+luc-github@users.noreply.github.com>
2022-09-22 10:15:04 +0800

    Add basic scripts for language packs management - WIP

---
+++ config/checkpack.js
@@ -0,0 +1,24 @@
+/*
+ chekpack.js - ESP3D WebUI package file
+
+ Copyright (c) 2021 Luc Lebosse. All rights reserved.
+ 
+ This code is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+ This code is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+ You should have received a copy of the GNU Lesser General Public
+ License along with This code; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+const chalk = require("chalk")
+const path = require("path")
+const fs = require("fs")
+const { Console } = require("console")
+const targetPath = path.normalize(__dirname + "/../languages/")
+
+console.log(red.green("Sorry script is not done yet, be patient"))
