f93c53953cf44367ea322fbe2c44f99374d3fd56
Luc <8822552+luc-github@users.noreply.github.com>
2022-10-09 12:49:45 +0800

    Code base for HP-GL WIP

---
+++ src/targets/Plotter/HP-GL/variablesTable.js
@@ -0,0 +1,41 @@
+/*
+ variablesTable.js - ESP3D WebUI helpers file
+
+ Copyright (c) 2021 Luc LEBOSSE. All rights reserved.
+ 
+ This code is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+ This code is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+ You should have received a copy of the GNU Lesser General Public
+ License along with This code; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+import { h } from "preact"
+
+const variablesTable = [
+    { name: "#pos_x#", value: 0 },
+    { name: "#pos_y#", value: 0 },
+    { name: "#pos_z#", value: 0 },
+    { name: "#pos_a#", value: 1 },
+    { name: "#pos_b#", value: 1 },
+    { name: "#pos_c#", value: 0 },
+    { name: "#pos_wx#", value: 0 },
+    { name: "#pos_wy#", value: 0 },
+    { name: "#pos_wz#", value: 0 },
+    { name: "#pos_wa#", value: 0 },
+    { name: "#pos_wb#", value: 0 },
+    { name: "#pos_wc#", value: 0 },
+    { name: "#prb_x#", value: 0 },
+    { name: "#prb_y#", value: 0 },
+    { name: "#prb_z#", value: 0 },
+    { name: "#laser_max#", value: 0 },
+    { name: "#selected_axis#", value: "X" },
+    { name: "#probe_thickness#", value: 0 },
+]
+
+export default variablesTable
