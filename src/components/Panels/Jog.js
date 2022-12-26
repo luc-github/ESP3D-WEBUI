9b0261eabe0ba068c3d8e7b655e2302fc1b70868
aaron GitHub <aaronse@outlook.com>
2022-12-26 14:05:27 -0800

    Implement key map overrides for jogging.  Enable Users to and adapt/define/suppress key shortcuts for their machine and keyboard setups
    Disable keyboard shortcuts when User is typing in Terminal (or any other text input field).  Suppress default behavior if key shortcut matched, fixes unexpected page scrolling.
    
    Reapply edits from 304378b and e4c369a
    
    Change Key Remapping config representation from string expression, to json fragment.  Luc indicated UI edits will be easier

--- src/components/Panels/Jog.js
+++ src/components/Panels/Jog.js
@@ -131,6 +131,50 @@ const JogPanel = () => {
     //keyboard listener handler
     const keyboardEventHandler = (e) => {
         if (!enable_keyboard_jog) RemoveKeyboardListener()
+
+        // Bail if User is actively typing text.  We don't want to disrupt them entering gcode.
+        if (document.activeElement
+            && document.activeElement.tagName == "INPUT" 
+            && document.activeElement.type == "text") {
+            console.log("Skipping key mapping, User is probably typing text");
+
+            return;
+        }
+
+        // Lookup and apply key map override(s).  Key map overrides are specified as a fragment.
+        // Use "NOP" as command Id to suppress a key from performing the default command action.
+        let keyMapObj = useUiContextFn.getValue("keymap");
+        if (keyMapObj && keyMapObj.length) {
+            let cmdMatch = keyMapObj.reduce( (acc, kv) => {
+                let iterKey = kv.value[0].value;
+                let iterId = kv.id;
+
+                if (iterKey == e.key) {
+                    return iterId;
+                }
+
+                return acc;
+            }, null);
+
+            if (cmdMatch) {
+
+                console.log("KeyMap override match, key = " + e.key + ", cmd= " + cmdMatch + ", alt= " + e.altKey);
+                
+                // Invoke commands not suppressed
+                if (cmdMatch.toUpperCase() != "NOP") {
+                    clickBtn(cmdMatch);
+                }
+                
+                // Suppress default key behavior.  For example, this prevents web page unexpectedly
+                // scrolling around when User jogs while keyboard shortcut mode is active
+                e.preventDefault();
+                
+                // Bail if command already matched and dispatched
+                return;
+            }
+        }
+
+        let hasKeyMatch = true;
         if (e.key == "ArrowRight") {
             clickBtn("btn+X")
         } else if (e.key == "ArrowLeft") {
@@ -167,7 +211,15 @@ const JogPanel = () => {
             clickBtn("move_1")
         } else if (e.key == "4") {
             clickBtn("move_0_1")
-        } else console.log(e.key)
+        } else {
+            hasKeyMatch = false;
+            console.log(e.key);
+        }
+
+        // Suppress default behavior if key press matched shortcut
+        if (hasKeyMatch) {
+            e.preventDefault();
+        }
     }
 
     //Add keyboard listener
