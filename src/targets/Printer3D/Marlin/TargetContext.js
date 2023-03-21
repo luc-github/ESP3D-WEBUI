203c4e8162996ea579610ffb9b2ffa8518d5f6ea
James Pearson <j.c.pearson@swansea.ac.uk>
2023-03-21 21:35:15 +0000

    Improves dispatchInternally() speed/reliability Introduce check at dispatchInternally() to do nothing if input is "ok". This prevents excessive parsing, that was casuing issues with temperature reporting.

--- src/targets/Printer3D/Marlin/TargetContext.js
+++ src/targets/Printer3D/Marlin/TargetContext.js
@@ -28,6 +28,7 @@ import { useDatasContext } from "../../../contexts"
 import { processor } from "./processor"
 import { isVerboseOnly } from "./stream"
 import {
+    isOk,
     isTemperatures,
     getTemperatures,
     isPositions,
@@ -171,7 +172,9 @@ const TargetContextProvider = ({ children }) => {
         //sensors
 
         if (type === "stream") {
-            if (isTemperatures(data)) {
+            if (isOk(data)) {
+            }else if (isTemperatures(data)) {
+            //if (isTemperatures(data)) {
                 const t = getTemperatures(data)
                 setTemperatures(t)
                 add2TemperaturesList({ temperatures: t, time: new Date() })
