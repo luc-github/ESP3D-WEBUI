74d5130d9823fce948b1c0c07074733a06ae5522
Luc <8822552+luc-github@users.noreply.github.com>
2023-12-04 09:14:42 +0800

    Bump version
    Build packages

--- src/components/App/version.js
+++ src/components/App/version.js
@@ -17,7 +17,7 @@
 */
 import { h } from 'preact'
 import { webUIbuild } from '../../targets'
-export const webUIversion = '3.0.0-a42'
+export const webUIversion = '3.0.0-a43'
 export const Esp3dVersion = () => (
     <span>
         {webUIversion}.{webUIbuild}
